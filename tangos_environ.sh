e6eea5cec29624dca46286bbf7b73583fc99902a
LilianChan22 <120372986@umail.ucc.ie>
2024-04-15 05:18:14 -0400

    first commit

---
+++ tangos_environ.sh
@@ -0,0 +1,3 @@
+export TANGOS_PROPERTY_MODULES=tangos_properties_tremmel,tangos_nbodyshop_properties
+#export TANGOS_DB_CONNECTION=/home/michael/Research/Data/Romulus/h1.cosmo50/Database/data_romulusC.db
+export TANGOS_DB_CONNECTION=/home/michael/Research/Data/Romulus/cosmo25/Database/data_romulus25.db
